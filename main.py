b91179c45e303a16100ba94d8c0a6b82a132184a
ToeiKanta <41673034+ToeiKanta@users.noreply.github.com>
2020-12-04 01:12:49 +0700

    add full page window

--- main.py
+++ main.py
@@ -47,14 +47,14 @@ class Application(tk.Frame):
         text_header.pack(side="top")
         var.set("PLEASE ENTER IMAGE NAME BELOW")
         # Text input
-        self.input_textbox = CustomText(self.input_frame, height=1, width=20, font=("Helvetica", 19), bg="gray", highlightthickness=0)
+        self.input_textbox = tk.Text(self.input_frame, height=1, width=20, font=("Helvetica", 19), bg="gray", highlightthickness=0)
         # text1.grid(sticky="n")
         self.input_textbox.pack(side="top")
         self.input_textbox.bind("<<TextModified>>", self.search_img)
         self.input_textbox.bind("<Return>", self.reset_text)
 
     def create_page_pagination(self):
-        paginationFrame = tk.Frame(root,pady=5)
+        paginationFrame = tk.Frame(root, pady=5)
         paginationFrame.pack()
         next_btn = tk.Label(paginationFrame, text="BACK", borderwidth=2, relief="raised", cursor="hand2")
         next_btn.bind("<Button-1>", self.backPage)
@@ -108,11 +108,21 @@ class Application(tk.Frame):
         self.input_textbox.insert("1.0", "")
         self.input = ""
 
-    def save_description(self, image_path, desc):
+    def save_description(self, image_path, string):
+        print("save: " + image_path + " :: " + string)
         img = pyexiv2.Image(image_path, encoding='utf-8')
-        userdata = {'Xmp.dc.desciption' : desc}
+        userdata = {'Xmp.dc.desciption' : string}
         img.modify_xmp(userdata)
         img.close()
+        self.reload_gallery()
+
+    def save_description_text_box(self, image_path, desc_text_component):
+        string = desc_text_component.get("1.0", "end-1c")
+        self.save_description(image_path, string)
+
+    def save_description_text_box_quit(self, image_path, desc_text_component, window):
+        self.save_description_text_box(image_path, desc_text_component)
+        window.destroy()
 
     def read_description(self, image_path):
         img = pyexiv2.Image(image_path, encoding='utf-8')
@@ -132,34 +142,72 @@ class Application(tk.Frame):
         self.create_gallery()
         self.current_page_label['text'] = " - page " + str(self.current_page_index) + " - "
 
+    def go_full_image(self, full_image_path):
+        # create new window
+        newWindow = tk.Toplevel(root)
+        newWindow.title(full_image_path)
+        newWindow.geometry("700x500")
+        # picture
+        origin = Image.open(full_image_path)
+        resized = origin.resize((500, 500), Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(resized)
+        img = tk.Label(newWindow, image=photo)
+        img.image = photo
+        img.pack(side="left")
+        # picture description
+        right_frame = tk.Frame(newWindow, padx=3)
+        right_frame.pack(side="left")
+        # image label
+        temp_text = "Image Path: \n" + full_image_path + "\n\nImage Description:"
+        image_label = tk.Label(right_frame, text=temp_text, justify="left", font=("Courier", 12))
+        image_label.pack(side="top",anchor="nw")
+        # description text area
+        desc = CustomText(right_frame, font=("Courier", 14))
+        desc.insert(tk.INSERT, self.read_description(full_image_path))
+        desc.pack(side="top")
+        desc.focus_set()
+        # save button
+        save_btn = tk.Label(right_frame,text="SAVE", borderwidth=2, relief="raised", cursor="hand2")
+        save_btn.bind("<Button-1>", lambda e, full_image_path=full_image_path, desc=desc: self.save_description_text_box(full_image_path, desc))
+        save_btn.pack(side="top", pady=2)
+        # save and quit button
+        save_quit_btn = tk.Label(right_frame, text="SAVE AND QUIT", borderwidth=2, relief="raised", cursor="hand2")
+        save_quit_btn.bind("<Button-1>", lambda e, full_image_path=full_image_path, desc=desc,newWindow=newWindow: self.save_description_text_box_quit(full_image_path, desc, newWindow))
+        save_quit_btn.pack(side="top", pady=2)
+        # quit button
+        quit_btn = tk.Label(right_frame, text="QUIT", borderwidth=2, relief="raised", cursor="hand2")
+        quit_btn.bind("<Button-1>", lambda e, newWindow=newWindow: newWindow.destroy())
+        quit_btn.pack(side="top", pady=2)
+
     def create_sub_gallery(self,frame, imagePath, imageName):
         frame = tk.Frame(frame)
-        frame.pack(side="left", padx=20, pady=20) # pack frame to gallery_frame
+        frame.pack(side="left", padx=5, pady=5) # pack frame to gallery_frame
         # picture
         origin = Image.open(imagePath)
         resized = origin.resize((100, 100), Image.ANTIALIAS)
         photo = ImageTk.PhotoImage(resized)
         img = tk.Label(frame, image=photo)
+        img.bind("<Button-1>", lambda e, imagePath=imagePath: self.go_full_image(imagePath))
         img.image = photo
         img.pack(side="top")
         # picture name
         var = tk.StringVar()
         text_header = tk.Label(frame, textvariable=var)
-        text_header.config(height=3)
+        text_header.config(height=4)
         text_header.pack(side="bottom")
         try:
             desc = self.read_description(imagePath)
         except:
             self.save_description(imagePath, "default description")
             desc = self.read_description(imagePath)
-        var.set(imageName + "\n" + desc)
+        var.set(imageName + "\n" + desc[:30] + "...")
 
 
     def create_gallery(self):
         self.gallery_frame = tk.Frame(root, width=100, height=100)
         self.gallery_frame.pack()
         dataPath = "./img"
-        print("=" + self.input + "=")
+        # print("=" + self.input + "=")
         start_i = (self.current_page_index-1) * (self.img_per_row * self.img_row_count)
         i = 0
         row = tk.Frame(self.gallery_frame)
@@ -176,19 +224,6 @@ class Application(tk.Frame):
                 if i > (self.current_page_index) * (self.img_per_row * self.img_row_count):
                     break
                 self.create_sub_gallery(row, os.path.join(dataPath, file), file)
-
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
-
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
 
 if __name__ == '__main__':
     root = tk.Tk()
