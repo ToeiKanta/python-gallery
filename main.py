1d415a857447c9143d0defa2059e8f02afe7075e
ToeiKanta <41673034+ToeiKanta@users.noreply.github.com>
2020-12-03 13:43:47 +0700

    add name search event

--- main.py
+++ main.py
@@ -5,10 +5,29 @@ import os
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class CustomText(tk.Text):
+    def __init__(self, *args, **kwargs):
+        """A text widget that report on internal widget commands"""
+        tk.Text.__init__(self, *args, **kwargs)
+
+        # create a proxy for the underlying widget
+        self._orig = self._w + "_orig"
+        self.tk.call("rename", self._w, self._orig)
+        self.tk.createcommand(self._w, self._proxy)
+
+    def _proxy(self, command, *args):
+        cmd = (self._orig, command) + args
+        result = self.tk.call(cmd)
+
+        if command in ("insert", "delete", "replace"):
+            self.event_generate("<<TextModified>>")
+
+        return result
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+        self.input = ""
         self.master = master
         self.pack()
         self.create_header()
@@ -19,20 +38,17 @@ class Application(tk.Frame):
         self.input_frame = tk.Frame(root, width=100, height=100)
         self.input_frame.pack()
         # Text input
-        text1 = tk.Text(self.input_frame, height=1, width=20, bg="gray", highlightthickness=0)
+        self.input_text = CustomText(self.input_frame, height=1, width=20, bg="gray", highlightthickness=0)
         # text1.grid(sticky="n")
-        text1.pack(side="top")
+        self.input_text.pack(side="top")
+        self.input_text.bind("<<TextModified>>", self.search)
 
-        # Search button
-        # btn = tk.Button(self, text="ค้นหา")
-        # btn["command"] = self.search("test")
-        # btn.pack(side="left")
+    def search(self, event):
+        self.input = event.widget.get("1.0", "end-1c")
+        self.gallery_frame.pack_forget()
+        self.create_gallery()
 
-    def search(self, text):
-        print("u search: " + text)
-
-
-    def create_sub_gallery(self, imagePath):
+    def create_sub_gallery(self, imagePath, imageName):
         frame = tk.Frame(self.gallery_frame)
         frame.pack(side="left", padx=20, pady=20) # pack frame to gallery_frame
         # picture
@@ -47,22 +63,23 @@ class Application(tk.Frame):
         text_header = tk.Label(frame, textvariable=var)
         text_header.config(height=1)
         text_header.pack(side="bottom")
-        var.set(imagePath)
+        var.set(imageName)
 
     def create_gallery(self):
         self.gallery_frame = tk.Frame(root,width=100, height=100)
         self.gallery_frame.pack()
         dataPath = "./img"
         for file in os.listdir(dataPath):
-            if file.endswith(".gif") or file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg"):
-                self.create_sub_gallery(os.path.join(dataPath, file))
+            if (file.endswith(".gif") or file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg"))\
+                    and self.input in file:
+                self.create_sub_gallery(os.path.join(dataPath, file),file)
 
     def create_header(self):
         var = tk.StringVar()
         text_header = tk.Label(self, textvariable=var)
         text_header.config(height=2)
         text_header.pack(side="top")
-        var.set("พิมพ์คำค้นหาที่ช่องด้านล่าง")
+        var.set("PLEASE ENTER IMAGE NAME")
 
 
     def create_widgets(self):
